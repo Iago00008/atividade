c91ffbefb24b85a720abc84e38db0b55d895568d
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-07 09:36:14 -0300

    06

--- Atividade 05/Program.cs
+++ Atividade 05/Program.cs
@@ -3,8 +3,9 @@
     public static void Main()
     {
         Console.WriteLine("Digite um qualquer número: ");
-        int num = Convert.ToInt32 ( Console.ReadLine());
+        int num = Convert.ToInt32(Console.ReadLine());
 
+        Console.Clear();
         if (num == 0)
         {
             Console.WriteLine("Este numero é zero");
---
+++ Atividade 06/Program.cs
@@ -0,0 +1,27 @@
+﻿
+class program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite sua nota:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+        if (num >= 9)
+        {
+            Console.WriteLine("Nota: A");
+        }
+        else if (num >= 7)
+        {
+            Console.WriteLine("Nota: B ");
+            
+        }
+        else if (num >= 5) 
+        {
+            Console.WriteLine("Nota: C");
+                }
+        else if (num <5) 
+        {
+            Console.WriteLine("Nota: D");
+                }
+    }
+}
