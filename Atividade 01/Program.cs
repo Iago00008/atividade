6986fde74dd201e47c51962815a1c660aec00c2b
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-07 08:30:59 -0300

    01

---
+++ Atividade 01/Program.cs
@@ -0,0 +1,19 @@
+﻿class program
+{
+    public static void Main()
+    {
+        //Atividade 01
+        Console.WriteLine("digite um número inteiro: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine($"O número {num} é Par!");
+
+        }
+        else
+        {
+            Console.WriteLine($"O número {num} é ímpar!");
+        }
+    }
+}
