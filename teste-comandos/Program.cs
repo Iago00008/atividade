76957c2d3f8043ae2dfd272449d040512aef159a
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-15 09:14:49 -0300

    ..

---
+++ teste-comandos/Program.cs
@@ -0,0 +1,27 @@
+﻿class program
+{
+    public static void Main()
+    {
+        bool maior = 20 > 10;
+        bool maiorOUigual = 20 >= 10;
+        bool menor = 20 < 30;
+        bool menorOUigual = 20 <= 30;
+        bool igual = 20 == 20;
+        bool diferente = 20 != 10;
+
+        Console.WriteLine(maior);
+        Console.WriteLine(maiorOUigual);
+        Console.WriteLine(menor);
+        Console.WriteLine(menorOUigual);
+        Console.WriteLine(igual);
+        Console.WriteLine(diferente);
+
+
+        Console.WriteLine("Digite um número: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        bool maiorDeIdade = idade >= 18;
+
+        Console.WriteLine(maiorDeIdade);
+    }
+}
