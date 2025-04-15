28d8707d45fff279d89fbeb23d69368b2c361a7f
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-15 09:27:01 -0300

    01

--- exercicio-01/Program.cs
+++ exercicio-01/Program.cs
@@ -1,4 +1,17 @@
-﻿class program
+﻿
+class Program
 {
-    public 
+    public static void Main()
+    {
+        Console.WriteLine("Digite um número:");
+        int num = Convert.ToInt32(Console.ReadLine());  
+
+        bool resultado = num >= 0;
+
+        if(resultado)
+        {
+            Console.WriteLine("Este número e Positivo.");
+        }
+
+    }
 }
